de153e594291bbc0e1cc907c62ea478cdf9513a4
Aarush <DksAarush@gmail.com>
2025-08-10 23:29:29 -0400

    working program.cs with tray icon

--- WebSearch/Program.cs
+++ WebSearch/Program.cs
@@ -8,10 +8,44 @@ namespace WebSearch
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new WebSearch());
+
+            // Create the main form
+            var mainForm = new WebSearch();
+
+            // Create NotifyIcon and context menu
+            var trayIcon = new NotifyIcon
+            {
+                Icon = new System.Drawing.Icon(Path.Combine(Constants.APP_DATA_FOLDER, Constants.App_Icon_File))
+,
+                Visible = true,
+                Text = "WebSearch"
+            };
+
+            var trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Exit", null, (s, e) =>
+            {
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+
+                if (!mainForm.IsDisposed)
+                    mainForm.Close();
+
+                Application.Exit();
+            });
+            trayIcon.ContextMenuStrip = trayMenu;
+
+            // Optionally: Hide the form on startup if you want it hidden
+            // mainForm.WindowState = FormWindowState.Minimized;
+            // mainForm.ShowInTaskbar = false;
+            // mainForm.Hide();
+
+            // Run the app with the main form and tray icon alive
+            Application.Run(mainForm);
+
+            // Cleanup tray icon after form closes
+            trayIcon.Dispose();
         }
+
     }
 }
